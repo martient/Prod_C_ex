3ee97f382121bd83be34bb6a517c096f75ba471f
martient <martient.pro@gmail.com>
2017-08-02 18:12:09 +0200

    Add files via upload

---
+++ ex0401/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main ()
+{
+    puts ("Ne viens pas m'importuner maintenant. J'ai du travail.");
+    puts("*Mange du chocolat* Le meilleur chocolat du MONDE !");
+    return (0);
+}
